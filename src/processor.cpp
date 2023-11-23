97427e137308ef7f88deec7455cafec93e8a155f
ISHADOW007 <149693065+ISHADOW007@users.noreply.github.com>
2023-11-23 20:28:17 +0530

    INX_impl instruction

--- src/processor.cpp
+++ src/processor.cpp
@@ -303,6 +303,19 @@ void Processor::tsx_impl(Memory *mem) {
     updateClock(mem->readMemVal(PC));
     UpdatePC(mem->readMemVal(PC));
 }
+void Processor::inx_impl(Memory *mem) {
+    X = X+1;
+
+    if(A==0) {
+        setZeroBit();
+    }
+    if(A<0) {
+        setNegativeBit();
+    }
+
+    updateClock(mem->readMemVal(PC));
+    UpdatePC(mem->readMemVal(PC));
+}
 
 
 
