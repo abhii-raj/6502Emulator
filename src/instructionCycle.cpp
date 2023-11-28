f0d0fb45c6c8f4b5fa09094caa7fb4404c7ec894
abhii-raj <abhishekrajgupta45@gmail.com>
2023-11-28 23:05:29 +0530

    Revert "instructions"
    This reverts commit 9a802f3faae7c01093e5668f974bcab3c0dc6b6b.

--- src/instructionCycle.cpp
+++ src/instructionCycle.cpp
@@ -54,57 +54,5 @@ void InstructionCycle::Execute(uint8_t opcode) {
         case 0xC9:
             proc.CMP_imdt(&mem);
             break;
-        case 0xC5:
-            proc.cmp_zpg(&mem);
-            break;
-        case 0xD5:
-            proc.cmp_zpgx(&mem);
-            break;
-        case 0xCD:
-            proc.cmp_abs(&mem);
-            break;
-        case 0x98:
-            proc.tya_impl(&mem);
-            break;
-        case 0x9A:
-            proc.txs_impl(&mem);
-            break;
-        case 0x8A:
-            proc.txa_impl(&mem);
-            break;
-        case 0x4C:
-            proc.jmp_abs(&mem);
-            break;
-        case 0x6C:
-            proc.jmp_idr(&mem);
-            break;
-        case 0xAA:
-            proc.tax_impl(&mem);
-            break;
-        case 0xA8:
-            proc.tay_impl(&mem);
-            break;
-        case 0xBA:
-            proc.tsx_impl(&mem);
-            break;
-        case 0xE8:
-            proc.inx_impl(&mem);
-            break;
-        case 0xC8:
-            proc.iny_impl(&mem);
-            break;
-        case 0X18:
-            proc.clc_impl(&mem);
-            break;
-        case 0xD8:
-            proc.cld_impl(&mem);
-            break;
-        case 0x58:
-            proc.cli_impl(&mem);
-            break;
-        case 0xB8:
-            proc.clv_impl(&mem);
-            break;
-
     }
 }
