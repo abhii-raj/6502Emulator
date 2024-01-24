a4d440adab651b44db39b7bde0111367f1a16612
shobhit <shobhit32t@gmail.com>
2024-01-25 01:44:13 +0530

    commented

--- asm_ex/POW.asm
+++ asm_ex/POW.asm
@@ -7,17 +7,17 @@ LDY #03         ; loaded value of exponent
 STY $0011       ; value of exponent(=3) is stored at 0011
                 ; at this point all the values stored in A, X, Y are safely stored in memory
                 ; and registers can be overwritten
-BEQ end         ; branch to end if exponent becomes 0
+BEQ end         ; branch to end if exponent is 0
 ; while(sty>0)
 ;    mul A * stx
-v: LDA #00         ; init for multiplication
+v: LDA #00      ; init for multiplication
 LDX $0007       ; value of acumulator
 STX $0005       ; stored value of counter for mul at 0005
-q: ADC $0015    ;
+q: ADC $0015
 DEC $0005
-BNE q
+BNE q           ; branch for repeated addition
 STA $0015
 DEC $0011
-BNE v
+BNE v           ; branch to keep multiplying till exponent becomes zero
 
 end: LDA $0015 ; final result is moved into A register
